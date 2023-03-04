45b49f07d0717dd29968eea2f128eaf1c1c8593f
Hung Pham <t.hungpham2314@gmail.com>
2023-03-04 11:10:05 +0700

    add tuan_5

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:tuan2/screens/album_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'dart:core';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final CollectionReference usersCollection =
+      FirebaseFirestore.instance.collection('users');
+  final CollectionReference productsCollection =
+      FirebaseFirestore.instance.collection('products');
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AlbumScreen(),
+    return MaterialApp(
+      title: 'Firestore Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Firestore Example'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Users'),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: usersCollection.snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        QueryDocumentSnapshot document =
+                            snapshot.data!.docs[index];
+                        return ListTile(
+                          title: Text(document['name']),
+                          subtitle: Text(document['email']),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Products'),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: productsCollection.snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        QueryDocumentSnapshot document =
+                            snapshot.data!.docs[index];
+                        return ListTile(
+                          title: Text(document['name']),
+                          subtitle: Text('Price: ${document['price']}'),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
